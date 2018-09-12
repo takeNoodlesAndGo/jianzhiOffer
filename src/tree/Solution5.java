8e0ace17548e419af6b66fa88e56655c6c7bf1b7
takeNoodlesAndGo <1163348439@qq.com>
2018-09-12 20:55:54 +0800

    28

---
+++ src/tree/Solution5.java
@@ -0,0 +1,33 @@
+package tree;
+
+import bean.TreeNode;
+
+public class Solution5 {
+	 public TreeNode lastNode = null;
+	 public TreeNode Convert(TreeNode pRootOfTree) {
+		 convertNode(pRootOfTree);
+		 lastNode = pRootOfTree;
+		 while(lastNode!=null&&lastNode.left!=null) {
+			 lastNode = lastNode.left;
+		 }
+		 return lastNode;
+	 }
+
+	private void convertNode(TreeNode node) {
+		// TODO Auto-generated method stub
+		if(node==null) {
+			return ;
+		}
+		if(node.left!=null) {
+			convertNode(node.left);
+		}
+		node.left = lastNode;
+		if(lastNode!=null) {
+			lastNode.right = node;
+		}
+		lastNode = node;
+		if(node.right!=null) {
+			convertNode(node.right);
+		}
+	}
+}
