a939e86323b0c9b28c5bb8776184df8d6467e6d6
takeNoodlesAndGo <1163348439@qq.com>
2018-09-18 17:13:54 +0800

    47

---
+++ src/unit6/Solution11.java
@@ -0,0 +1,15 @@
+package unit6;
+
+public class Solution11 {
+	 public int Add(int num1,int num2) {
+	      int sum = 0;
+	      int carry = 0;
+	      do {
+	    	  sum = num1^num2;
+	    	  carry = (num1&num2)<<1;
+	    	  num1 = sum;
+	    	  num2 = carry;
+	      }while(num2!=0);
+	      return num1;
+	 }
+}
